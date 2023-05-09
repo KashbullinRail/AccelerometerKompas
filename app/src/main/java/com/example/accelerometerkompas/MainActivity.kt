31e7dc7e9e9e07cc918ad2aa9cbc3598aa0c7d7e
KashbullinRail <kashbullinrail@gmail.com>
2023-05-09 16:24:06 +0300

    переделал в компас

--- app/src/main/java/com/example/accelerometerkompas/MainActivity.kt
+++ app/src/main/java/com/example/accelerometerkompas/MainActivity.kt
@@ -6,51 +6,91 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var sManager: SensorManager
+    private lateinit var sensorManager: SensorManager
+    private var sensor: Sensor? = null
+    private var sensor2: Sensor? = null
+//    private val sListner by lazy { SensorEventListener() }
     private var magnit = FloatArray(9)
     private var gravity = FloatArray(9)
     private var acceler = FloatArray(3)
     private var magnetfield = FloatArray(3)
     private var values = FloatArray(3)
 
+//    private lateinit var sensorManager: SensorManager
+//    private val accelerometerReading = FloatArray(3)
+//    private val magnetometerReading = FloatArray(3)
+//
+//    private val rotationMatrix = FloatArray(9)
+//    private val orientationAngles = FloatArray(3)
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
         val tvSensor = findViewById<TextView>(R.id.tvSensor)
         val lRotation = findViewById<LinearLayout>(R.id.lRotation)
+//
+//        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val sensor2 = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+//
+//        val sensor2 = sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION)
 
-        sManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val sensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        val sensor2 = sManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+//        private lateinit var sensorManager: SensorManager
+//        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+//        if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+//            // Success! There's a magnetometer.
+//        } else {
+//            // Failure! No magnetometer.
+//        }
 
         val sListner = object : SensorEventListener {
             override fun onSensorChanged(event: SensorEvent?) {
+                var rotateMiddle = 0.0
                 when (event?.sensor?.type) {
                     Sensor.TYPE_ACCELEROMETER -> acceler = event.values.clone()
                     Sensor.TYPE_MAGNETIC_FIELD -> magnetfield = event.values.clone()
+                    Sensor.TYPE_ORIENTATION -> magnetfield = event.values.clone()
                 }
+
                 SensorManager.getRotationMatrix(gravity, magnit, acceler, magnetfield)
                 val outGravity = FloatArray(9)
                 SensorManager.remapCoordinateSystem(
                     gravity,
-                    SensorManager.AXIS_X,
+                    SensorManager.AXIS_Y,
                     SensorManager.AXIS_Z,
                     outGravity
                 )
-                SensorManager.getOrientation(outGravity, values)
+//                SensorManager.getOrientation(outGravity, values)
+                SensorManager.getOrientation(gravity, values)
 
-                val degree = Math.toDegrees(values[2].toDouble())
-                lRotation.rotation = degree.toFloat() + 270
-                val rotateMiddle = degree + 90
+                val degree = Math.toDegrees(values[0].toDouble())
+
+                rotateMiddle = when(degree.toInt()) {
+                   in 0..180 -> degree
+                   in -180..0 -> degree + 360
+                    else -> { degree }
+                }
+
+//                if (degree < 0) {
+////                    lRotation.rotation = degree.toFloat() + 270
+//                    var rotateMiddle =  degree + 360
+//                } else {
+//                    rotateMiddle = degree
+//                }
+
 
                 val colorLayout = if(rotateMiddle.toInt() == 0) {
                     Color.GREEN
@@ -59,15 +99,37 @@ class MainActivity : AppCompatActivity() {
                 }
                 lRotation.setBackgroundColor(colorLayout)
                 tvSensor.text = rotateMiddle.toInt().toString()
-            }
 
+
+            }
+//
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
             }
-
+//
         }
-        sManager.registerListener(sListner, sensor, SensorManager.SENSOR_DELAY_NORMAL)
-        sManager.registerListener(sListner, sensor2, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(sListner, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(sListner, sensor2, SensorManager.SENSOR_DELAY_NORMAL)
 
     }
+
+    override fun onResume() {
+        super.onResume()
+//        sensor?.also { sensor ->
+//            sensorManager.registerListener(sListner, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+//        }
+//        sensor2?.also { sensor ->
+//            sensorManager.registerListener(this, sensor2, SensorManager.SENSOR_DELAY_NORMAL)
+//        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+//        sensorManager.unregisterListener(this)
+
+    }
+
+
+
 
 }
